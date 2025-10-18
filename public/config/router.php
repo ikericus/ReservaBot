210bb6e83ce993a42a7a182d1f643c83c38dc1c1
ikericus <iker.zuazu@proton.me>
2025-10-18 11:35:27 +0200

    v0

--- public/config/router.php
+++ public/config/router.php
@@ -21,6 +21,7 @@ class Router {
     private function defineRoutes() {
                 
         // Rutas públicas
+        $this->addRoute('GET', '/test-simple', 'pages/test-simple.php');
         $this->addRoute('GET',      '/test',                    'pages/test.php');
         $this->addRoute('GET',      '/',                        'pages/home.php');
         $this->addRoute('GET',      '/landing',                 'pages/landing.php');
