4e0a61ec169caeec93ebada97835548399ee08a1
ikericus <iker.zuazu@proton.me>
2025-10-18 11:39:49 +0200

    Update login.php

--- public/pages/login.php
+++ public/pages/login.php
@@ -1,9 +1,4 @@
 <?php
-// Iniciar sesión para manejar mensajes
-session_start();
-
-require_once dirname(__DIR__) . '/includes/db-config.php';
-require_once dirname(__DIR__) . '/includes/auth.php';
 
 // Si ya está autenticado, redirigir al inicio
 if (isAuthenticated()) {
