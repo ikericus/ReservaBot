ccf0a0fae51d2ee18dccbf7b7a84e85779b06dbd
ikericus <iker.zuazu@proton.me>
2025-10-19 17:30:52 +0200

    PROJECT_ROOT a bootstrap

--- public/index.php
+++ public/index.php
@@ -5,16 +5,13 @@
  * Punto de entrada principal de ReservaBot
  */
 
-// Definir raíz del proyecto (public/)
-define('PROJECT_ROOT', __DIR__);
-
 // Mostrar todos los errores en pantalla
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 
 // Incluir router
-require_once PROJECT_ROOT . '/config/router.php';
+require_once  __DIR__ . '/config/router.php';
 
 // Ejecutar
 $router = new Router();
