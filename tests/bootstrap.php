1de6a7562a390bbb5f57a3996f745588f552689b
ikericus <iker.zuazu@proton.me>
2025-10-21 18:12:13 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
+
+// Definir raíz del proyecto (public/)
+define('PROJECT_ROOT', dirname(__DIR__) . '/public');
+
 // Cargar funciones globales y configuración
 require_once __DIR__ . '/../public/config/bootstrap.php';
