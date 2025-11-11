4e0f330d1c0a6e88771c52de0878ac87decc7f74
ikericus <iker.zuazu@proton.me>
2025-11-11 15:58:56 +0100

    Prueba workflows

--- public/config/router.php
+++ public/config/router.php
@@ -59,7 +59,7 @@ class Router {
         $this->addRoute('GET',      '/reserva',                         'pages/reserva/reserva.php',            ['auth']);        
         $this->addRoute('GET',      '/reserva-form',                    'pages/reserva/reserva-form.php',       ['auth']);  
         $this->addRoute('GET',      '/clientes',                        'pages/cliente/clientes.php',           ['auth']);
-        $this->addRoute('GET',      '/cliente',                         'pages/cliente/cliente.php',    ['auth']);
+        $this->addRoute('GET',      '/cliente',                         'pages/cliente/cliente.php',            ['auth']);
         $this->addRoute('GET',      '/formularios',                     'pages/formulario/formularios.php',     ['auth']);
         $this->addRoute('POST',     '/formularios',                     'pages/formulario/formularios.php',     ['auth']); 
         $this->addRoute('GET',      '/configuracion',                   'pages/user/configuracion.php',         ['auth']);
